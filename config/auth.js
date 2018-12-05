351a50bf7b67f22db0666d70ebb352bb7eacc1db
Nick Scherman <n_scherman@hotmail.com>
2018-12-04 19:36:59 -0500

    auth file

---
+++ config/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "API_KEY": "51409829fde7301f92764587ba6d2b6d",
+    "API_SECRET": "",
+    "APP_URL": "team-13-fe.azurewebsites.net",
+    "REDIRECT_URI": "https://team-13-shopify.azurewebsites.net"
+};
